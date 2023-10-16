5f93b9f9cad61f3cd1fcc76596debad146f0f30b
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-10-16 07:43:09 +0400

    updated the installation files

--- install/nexus/nexus-install.sh
+++ install/nexus/nexus-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
+apt-get install java-1.8.0-openjdk.x86_64 wget -y   
 mkdir -p /opt/nexus/   
 mkdir -p /tmp/nexus/                           
 cd /tmp/nexus/
